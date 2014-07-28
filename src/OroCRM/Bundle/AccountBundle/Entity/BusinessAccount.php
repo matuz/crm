5d76c7313371f3ef36d9255efc203ea873036467
j.martuzalski <martuzalski@gmail.com>
2014-07-28 13:47:22 +0200

    Rozdzielenie typów kontrahentów

---
+++ src/OroCRM/Bundle/AccountBundle/Entity/BusinessAccount.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace OroCRM\Bundle\AccountBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class BusinessAccount
+ * @package OroCRM\Bundle\AccountBundle\Entity
+ *
+ * @ORM\Entity()
+ */
+class BusinessAccount extends Account{
+
+} 
---
+++ src/OroCRM/Bundle/AccountBundle/Entity/IndividualAccount.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace OroCRM\Bundle\AccountBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class IndividualAccount
+ * @package OroCRM\Bundle\AccountBundle\Entity
+ *
+ * @ORM\Entity()
+ */
+class IndividualAccount extends Account{
+
+} 
